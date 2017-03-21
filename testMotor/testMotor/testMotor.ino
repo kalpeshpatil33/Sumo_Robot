a34f8431eac025d66d465ecf45bb72649f5292bc
kalpesh <kalpeshpatil33@gwu.edu>
2017-03-21 14:52:12 -0400

    testMotor updated with comments

--- testMotor/testMotor/testMotor.ino
+++ testMotor/testMotor/testMotor.ino
@@ -1,10 +1,18 @@
-const int switchPin = LOW;    // switch input
+/*
+ * 
+ * Motor Testing Code
+ * Motor1 +: 1Y -:2Y (motor1Pin,motor2Pin)
+ * Motor2 +: 4Y -:3Y (motor3Pin,motor4Pin)
+ * 
+ * 
+ */
+
 const int motor1Pin = 3;    // H-bridge leg 1 (pin 2, 1A)
 const int motor2Pin = 4;    // H-bridge leg 2 (pin 7, 2A)
 const int enablePin1 = 9;    // H-bridge enable pin
 
-const int motor3Pin = 5;    // H-bridge leg 1 (pin 2, 1A)
-const int motor4Pin = 6;    // H-bridge leg 2 (pin 7, 2A)
+const int motor3Pin = 5;    // H-bridge leg 1 (pin 10, 3A)
+const int motor4Pin = 6;    // H-bridge leg 2 (pin 15, 4A)
 const int enablePin2 = 10;    // H-bridge enable pin
 
 void setup() {
@@ -19,10 +27,6 @@ void setup() {
   pinMode(enablePin2, OUTPUT);
 
   Serial.begin(9600);
-
-  // set enablePin high so that motor can turn on:
-  digitalWrite(enablePin1, HIGH);
-  digitalWrite(enablePin2, HIGH);
 }
 
 void loop() {
@@ -32,21 +36,51 @@ void loop() {
     int n = Serial.readStringUntil('\n').toInt();
 
     if (n == 1) {
-      Serial.print("Forward");
+      Serial.println("Forward");
+      digitalWrite(enablePin1, HIGH);
+      digitalWrite(enablePin2, HIGH);
       digitalWrite(motor1Pin, LOW);   // set leg 1 of the H-bridge low
       digitalWrite(motor2Pin, HIGH);  // set leg 2 of the H-bridge high
 
       digitalWrite(motor3Pin, HIGH);   // set leg 1 of the H-bridge low
       digitalWrite(motor4Pin, LOW);  // set leg 2 of the H-bridge high
     }
-    // if the switch is low, motor will turn in the other direction:
+
     if (n == 2) {
-      Serial.print("Backward");
+      Serial.println("Backward");
+      digitalWrite(enablePin1, HIGH);
+      digitalWrite(enablePin2, HIGH);
       digitalWrite(motor1Pin, HIGH);  // set leg 1 of the H-bridge high
       digitalWrite(motor2Pin, LOW);   // set leg 2 of the H-bridge low
 
       digitalWrite(motor3Pin, LOW);  // set leg 1 of the H-bridge high
-      digitalWrite(motor4Pin, HIGH);   // set leg 2 of the H-bridge low
+      digitalWrite(motor4Pin, HIGH);
+    }
+    if (n == 3) {
+      Serial.println("LEFT");
+      digitalWrite(enablePin1, HIGH);
+      digitalWrite(enablePin2, HIGH);
+      digitalWrite(motor1Pin, LOW);   // set leg 1 of the H-bridge low
+      digitalWrite(motor2Pin, HIGH);  // set leg 2 of the H-bridge high
+
+      digitalWrite(motor3Pin, LOW);   // set leg 1 of the H-bridge low
+      digitalWrite(motor4Pin, HIGH);  // set leg 2 of the H-bridge high
+    }
+    if (n == 4) {
+      Serial.println("RIGHT");
+      digitalWrite(enablePin1, HIGH);
+      digitalWrite(enablePin2, HIGH);
+      digitalWrite(motor1Pin, HIGH);   // set leg 1 of the H-bridge low
+      digitalWrite(motor2Pin, LOW);  // set leg 2 of the H-bridge high
+
+      digitalWrite(motor3Pin, HIGH);   // set leg 1 of the H-bridge low
+      digitalWrite(motor4Pin, LOW);  // set leg 2 of the H-bridge high
+    }
+
+    if (n == 5) {
+      Serial.println("STOP");
+      digitalWrite(enablePin1, LOW);
+      digitalWrite(enablePin2, LOW);
     }
   }
 }
