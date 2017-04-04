c1dbabb84024acb0ff549d1fa3ac631376038a63
kalpesh <kalpeshpatil33@gwu.edu>
2017-04-04 13:28:16 -0400

    update testMotor

--- testMotor/testMotor/testMotor.ino
+++ testMotor/testMotor/testMotor.ino
@@ -1,124 +1,61 @@
-/*
-   Kalpesh Patil
-   Motor Testing Code
-   Motor1 +: 1Y -:2Y (motor1Pin,motor2Pin)
-   Motor2 +: 4Y -:3Y (motor3Pin,motor4Pin)
-
-
-*/
-
-const int motor1Pin = 3;    // H-bridge leg 1 (pin 2, 1A)
-const int motor2Pin = 4;    // H-bridge leg 2 (pin 7, 2A)
-const int enablePin1 = 9;    // H-bridge enable pin
-
-const int motor3Pin = 5;    // H-bridge leg 1 (pin 10, 3A)
-const int motor4Pin = 6;    // H-bridge leg 2 (pin 15, 4A)
-const int enablePin2 = 10;    // H-bridge enable pin
-
-unsigned long ntime;
+#include <Servo.h>
+Servo myservo1;// create servo object to control a servo
+Servo myservo2;
 
 void goForward()
 {
   Serial.println("Forward");
-  digitalWrite(enablePin1, HIGH);
-  digitalWrite(enablePin2, HIGH);
-  digitalWrite(motor1Pin, LOW);   // set leg 1 of the H-bridge low
-  digitalWrite(motor2Pin, HIGH);  // set leg 2 of the H-bridge high
-
-  digitalWrite(motor3Pin, HIGH);   // set leg 1 of the H-bridge low
-  digitalWrite(motor4Pin, LOW);  // set leg 2 of the H-bridge high
+  myservo1.write(0);
+  myservo2.write(0);
 }
 
 void goBackward()
 {
   Serial.println("Backward");
-  digitalWrite(enablePin1, HIGH);
-  digitalWrite(enablePin2, HIGH);
-  digitalWrite(motor1Pin, HIGH);  // set leg 1 of the H-bridge high
-  digitalWrite(motor2Pin, LOW);   // set leg 2 of the H-bridge low
-
-  digitalWrite(motor3Pin, LOW);  // set leg 1 of the H-bridge high
-  digitalWrite(motor4Pin, HIGH);
+  myservo1.write(180);
+  myservo2.write(180);
 }
 
 void goLeft()
 {
-  Serial.println("LEFT");
-  digitalWrite(enablePin1, HIGH);
-  digitalWrite(enablePin2, HIGH);
-  digitalWrite(motor1Pin, LOW);   // set leg 1 of the H-bridge low
-  digitalWrite(motor2Pin, HIGH);  // set leg 2 of the H-bridge high
-
-  digitalWrite(motor3Pin, LOW);   // set leg 1 of the H-bridge low
-  digitalWrite(motor4Pin, HIGH);  // set leg 2 of the H-bridge high
+  Serial.println("Left");
+  myservo1.write(0);
+  myservo2.write(180);
 }
+
 void goRight()
 {
-  Serial.println("RIGHT");
-  digitalWrite(enablePin1, HIGH);
-  digitalWrite(enablePin2, HIGH);
-  digitalWrite(motor1Pin, HIGH);   // set leg 1 of the H-bridge low
-  digitalWrite(motor2Pin, LOW);  // set leg 2 of the H-bridge high
-
-  digitalWrite(motor3Pin, HIGH);   // set leg 1 of the H-bridge low
-  digitalWrite(motor4Pin, LOW);  // set leg 2 of the H-bridge high
+  Serial.println("Right");
+  myservo1.write(180);
+  myservo2.write(0);
 }
 
 void goStop()
 {
-  Serial.println("STOP");
-  digitalWrite(enablePin1, LOW);
-  digitalWrite(enablePin2, LOW);
+  Serial.println("Stop");
+  myservo1.write(95);
+  myservo2.write(95);
 }
 
-void setup() {
-
-  // set all the other pins you're using as outputs:
-  pinMode(motor1Pin, OUTPUT);
-  pinMode(motor2Pin, OUTPUT);
-  pinMode(enablePin1, OUTPUT);
-
-  pinMode(motor3Pin, OUTPUT);
-  pinMode(motor4Pin, OUTPUT);
-  pinMode(enablePin2, OUTPUT);
+void setup()
+{
+  myservo1.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo2.attach(8);
 
   Serial.begin(9600);
-
-  goForward();
-  delay(1000);
-  goBackward();
-  delay(1000);
-  goLeft();
-  delay(1000);
-  goRight();
-  delay(1000);
   goStop();
-
 }
 
-void loop() {
-
-  ntime = millis();
-
-    if (Serial.available() > 0 )
-  {
-    int n = Serial.readStringUntil('\n').toInt();
-
-    if (n == 1)
-    {
-    }
-    if (n == 2)
-    {
-    }
-    if (n == 3)
-    {
-    }
-    if (n == 4)
-    {
-    }
-
-    if (n == 5)
-    {
-    }
-  }
+void loop()
+{
+  goForward();
+  delay(5000);
+  goBackward();
+  delay(5000);
+  goLeft();
+  delay(5000);
+  goRight();
+  delay(5000);
+  goStop();
+  delay(5000); 
 }
--- testMotor_QTI/testMotor_QTI.ino
+++ testMotor_QTI/testMotor_QTI.ino
@@ -1,76 +1,61 @@
-/*
-   Kalpesh Patil
-   Motor Testing Code with QTI Sensor
-   Motor1 +: 1Y -:2Y (motor1Pin,motor2Pin)
-   Motor2 +: 4Y -:3Y (motor3Pin,motor4Pin)
-  QTI Sensor as an analog input
-   W: Ein
-   R: I/O: A4
-   B: GND
+#include <Servo.h>
+Servo myservo1;// create servo object to control a servo
+Servo myservo2;
 
-*/
+void goForward()
+{
+  Serial.println("Forward");
+  myservo1.write(0);
+  myservo2.write(0);
+}
 
-const int motor1Pin = 3;    // H-bridge leg 1 (pin 2, 1A)
-const int motor2Pin = 4;    // H-bridge leg 2 (pin 7, 2A)
-const int enablePin1 = 9;    // H-bridge enable pin
+void goBackward()
+{
+  Serial.println("Backward");
+  myservo1.write(180);
+  myservo2.write(180);
+}
 
-const int motor3Pin = 5;    // H-bridge leg 1 (pin 10, 3A)
-const int motor4Pin = 6;    // H-bridge leg 2 (pin 15, 4A)
-const int enablePin2 = 10;    // H-bridge enable pin
+void goLeft()
+{
+  Serial.println("Left");
+  myservo1.write(0);
+  myservo2.write(180);
+}
 
-int sensorVal; //
-const int sensor = A4; //
+void goRight()
+{
+  Serial.println("Right");
+  myservo1.write(180);
+  myservo2.write(0);
+}
 
-void setup() {
+void goStop()
+{
+  Serial.println("Stop");
+  myservo1.write(95);
+  myservo2.write(95);
+}
 
-  // set all the other pins you're using as outputs:
-  pinMode(motor1Pin, OUTPUT);
-  pinMode(motor2Pin, OUTPUT);
-  pinMode(enablePin1, OUTPUT);
-
-  pinMode(motor3Pin, OUTPUT);
-  pinMode(motor4Pin, OUTPUT);
-  pinMode(enablePin2, OUTPUT);
-
-  pinMode(sensor, INPUT); //set as input so we can perform a read on the pin
+void setup()
+{
+  myservo1.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo2.attach(8);
 
   Serial.begin(9600);
-  digitalWrite(sensor, HIGH);
+  goStop();
 }
-void rotateLeft()
+
+void loop()
 {
-  Serial.println("LEFT");
-  digitalWrite(enablePin1, HIGH);
-  digitalWrite(enablePin2, HIGH);
-  digitalWrite(motor1Pin, LOW);   // set leg 1 of the H-bridge low
-  digitalWrite(motor2Pin, HIGH);  // set leg 2 of the H-bridge high
-
-  digitalWrite(motor3Pin, LOW);   // set leg 1 of the H-bridge low
-  digitalWrite(motor4Pin, HIGH);  // set leg 2 of the H-bridge high
-}
-
-void rotateStop()
-{
-  Serial.println("STOP");
-  digitalWrite(enablePin1, LOW);
-  digitalWrite(enablePin2, LOW);
-}
-void loop() {
-  // if the switch is high, motor will turn on one direction:
-
-
-  sensorVal = analogRead(sensor); //save the value from the analog read into sensorVal
-  Serial.println(sensorVal); //display sensorVal
-
-  if (sensorVal < 100)
-  {
-    Serial.println("Rotating left; START");
-    rotateLeft();
-    delay(500);
-  }
-  else
-  {
-    rotateStop();
-    Serial.println("Finished Rotating left; STOP");
-  }
+  goForward();
+  delay(5000);
+  goBackward();
+  delay(5000);
+  goLeft();
+  delay(5000);
+  goRight();
+  delay(5000);
+  goStop();
+  delay(5000); 
 }
