b10a4512401bb3ee349cfcb8a4019629c7c5fb03
Kanishke-XPS <kanishkegb@gwu.edu>
2017-05-08 09:16:34 -0400

    LCD works

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -1,7 +1,6 @@
 #include <LiquidCrystal.h>// include the library code
 /**********************************************************/
-char array1[] = " SunFounder               "; //the string to print on the LCD
-char array2[] = "hello, world!             "; //the string to print on the LCD
+char array1[] = " Bot Info               "; //the string to print on the LCD
 
 int tim = 200; //the value of delay timef
 // initialize the library with the numbers of the interface pins
@@ -14,6 +13,24 @@ void setup()
   Serial1.begin(115200);
   lcd.begin(16, 2); // set up the LCD's number of columns and rows:
   lcd.setCursor(15, 0); // set the cursor to column 15, line 0
+
+  while (Serial.available() > 0)
+    Serial1.read();
+
+//  lcd.scrollDisplayLeft();
+//  lcd.print(array1[positionCounter1]); // Print a message to the LCD.
+//  delay(tim); //delay
+//  positionCounter1++;
+
+  lcd.clear();
+  lcd.setCursor(15, 0);
+  positionCounter1 = 0;
+  while (positionCounter1 < 15) {
+    lcd.scrollDisplayLeft();
+    lcd.print(array1[positionCounter1]);
+    delay(tim);
+    positionCounter1++;
+  }
 }
 /*********************************************************/
 void loop()
@@ -23,24 +40,47 @@ void loop()
     in = Serial1.readStringUntil('\n');
     Serial.println(in);
   }
-  lcd.scrollDisplayLeft();
-  lcd.print(array1[positionCounter1]); // Print a message to the LCD.
-  delay(tim); //delay
-  positionCounter1++;
-
-  if (positionCounter1 > 15) {
-    positionCounter1 = 0;
-    lcd.clear();
-    lcd.setCursor(15, 0); // set the cursor to column 15, line 0
-  }
+  int in_int = in.toInt();
 
   lcd.setCursor(0, 1);
-  lcd.print(in);
+  lcd.clear();
+  switch (in_int) {
+    case 1:
+      Serial.println(in_int);
+      lcd.print("Forward      ");
+      break;
+    case 2:
+      lcd.print("Backward      ");
+      break;
+    case 3:
+      lcd.print("Left      ");
+      break;
+    case 4:
+      lcd.print("Right      ");
+      break;
+    case 5:
+      lcd.print("Stop      ");
+      break;
+    case 6:
+      lcd.print("Attack      ");
+      break;
+    case 7:
+      lcd.print("Searching      ");
+      break;
+    case 10:
+      lcd.print("Shutdown      ");
+      break;
+    default:
+      break;
+  }
+
+
+  //  lcd.print(in_int);
   delay(500);
 
   while (Serial.available() > 0)
     Serial1.read();
-    
+
   //  lcd.setCursor(15, 0); // set the cursor to column 15, line 0
   //  for ( int positionCounter1 = 0; positionCounter1 < 26; positionCounter1++)
   //  {
